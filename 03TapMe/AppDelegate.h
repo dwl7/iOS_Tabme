96728fe202c7ae3c1c7753dfd9f9f1dd37241fa0
dwl7 <dwl7@sfu.ca>
2015-03-20 03:29:38 -0700

    Initial Commit

---
+++ 03TapMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03TapMe
+//
+//  Created by Dae Woong Lim on 2015-03-20.
+//  Copyright (c) 2015 Dae Woong Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 03TapMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03TapMe
+//
+//  Created by Dae Woong Lim on 2015-03-20.
+//  Copyright (c) 2015 Dae Woong Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
